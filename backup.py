4f1ebb6c9d9b8d7c98ffe7f43380e3bb6728638e
João Fernandes <joaofilho8274@gmail.com>
2025-02-11 14:49:17 -0300

    Caminho da pasta no arquivo config.yaml

--- backup.py
+++ backup.py
@@ -1,23 +1,24 @@
-import argparse
 import subprocess
+import re
 
 def backup(path):
     comand = ["kopia", "snapshot", "create", path] # Comando para fazer o backup
     subprocess.run(comand) # Executa o comando no terminal
 
-def arguments(): # Pega os argumentos passados pelo terminal
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", required=True)
-    return parser.parse_args()
+def arguments(): # Pega os argumentos passados no arquivo config.yaml
+    with open("./config.yaml", 'r', encoding='utf-8') as file:
+        text = file.read() # Pega o conteúdo do arquivo
+    regex = r'path:\s*"(.*)"' # Regex para pegar o caminho da pasta
+    result = re.search(regex, text) # Procura o caminho da pasta no arquivo
+    return result.group(1) # Retorna o caminho da pasta
 
 def main():
     try:
-        args = arguments()
-        path = args.path # Caminho da pasta para fazer o backup
+        path = arguments() # Caminho da pasta para fazer o backup
         backup(path)
     except Exception as e:
         print("Erro:", e)
 
-# python backup.py -p "caminho/da/pasta"
+# python backup.py
 if __name__ == '__main__':
     main()
