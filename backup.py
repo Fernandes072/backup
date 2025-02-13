74b2609560e7c4dc842a499c23b842cdbe64751b
João Fernandes <joaofilho8274@gmail.com>
2025-02-12 23:46:07 -0300

    Não precisa baixar o Kopia e sincroniza com o Google Drive

--- backup.py
+++ backup.py
@@ -1,21 +1,34 @@
 import subprocess
 import re
 
-def backup(path):
-    comand = ["kopia", "snapshot", "create", path] # Comando para fazer o backup
+def drive(path): # Sincroniza o backup local com o Google Drive
+    comand = ["./rclone", "sync", "-v", path, "gdrive:/Backup"] # Comando para sincronizar o backup
+    subprocess.run(comand) # Executa o comando no terminal
+
+def backup(path): # Faz o backup local
+    comand = ["./kopia", "snapshot", "create", path] # Comando para fazer o backup
     subprocess.run(comand) # Executa o comando no terminal
 
 def arguments(): # Pega os argumentos passados no arquivo config.yaml
+    arguments = []
     with open("./config.yaml", 'r', encoding='utf-8') as file:
         text = file.read() # Pega o conteúdo do arquivo
+
     regex = r'path:\s*"(.*)"' # Regex para pegar o caminho da pasta
     result = re.search(regex, text) # Procura o caminho da pasta no arquivo
-    return result.group(1) # Retorna o caminho da pasta
+    arguments.append(result.group(1))
+
+    regex = r'repository:\s*"(.*)"' # Regex para pegar o caminho do repositorio
+    result = re.search(regex, text) # Procura o caminho do repositorio no arquivo
+    arguments.append(result.group(1))
+
+    return arguments
 
 def main():
     try:
-        path = arguments() # Caminho da pasta para fazer o backup
-        backup(path)
+        args = arguments()
+        backup(args[0])
+        drive(args[1])
     except Exception as e:
         print("Erro:", e)
 
